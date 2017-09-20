9a545a40ab3a627d7dad3ac40546af706795c9e0
Alberto Xamin <alberto@xamin.it>
2017-09-20 18:01:27 +0200

    auto wake

--- index.js
+++ index.js
@@ -6,3 +6,6 @@ http.createServer(function (request, response) {
        
      }).listen(process.env.PORT || 5000);     
 
+     setInterval(function() {
+         http.get("https://unimeal-bot.herokuapp.com/");
+     }, 200000); // every 5 minutes (300000)
